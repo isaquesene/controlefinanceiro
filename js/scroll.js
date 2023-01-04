bd47e58b7e1bbaab9917f9792a04fe8e809f4d56
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2023-01-04 19:35:02 -0300

    atualizar

--- js/scroll.js
+++ js/scroll.js
@@ -13,7 +13,7 @@ let calcScrollValue = () =>{
     //console.log(scrollValue);
 
     if(pos > 100){
-        scrollProgress.style.display = "inline-block";
+        scrollProgress.style.display = "grid";
     }else{
         scrollProgress.style.display = "none";
     }
